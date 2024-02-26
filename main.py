f7136a4b23031fea1058f569b9204ff17696f3d8
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-02-26 02:18:20 +0100

    feat: get word count

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
     path = "./books/frankenstein.txt"
     text = read_text_from_file(path)
+    word_count = get_word_count(text)
 
 def read_text_from_file(path):
     with open(path) as f:
         return f.read()
+
+def get_word_count(text):
+    return len(text.split())
