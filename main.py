1427bdcaa039a668fc51bb3743cf2ec3f2be0706
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-02-26 02:33:45 +0100

    feat: get char occurrence dict

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ def main():
     path = "./books/frankenstein.txt"
     text = read_text_from_file(path)
     word_count = get_word_count(text)
+    char_dict = get_char_dict(text)
+
 
 def read_text_from_file(path):
     with open(path) as f:
         return f.read()
 
+
 def get_word_count(text):
     return len(text.split())
+
+
+def get_char_dict(text):
+    chars_dict = {}
+    for char in text.lower():
+        if not char.isalpha():
+            continue
+        chars_dict[char] = chars_dict.get(char, 0) + 1
+    return chars_dict
